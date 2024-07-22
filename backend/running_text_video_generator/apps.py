b3db075885e2c6457ff0a3e9d7af86ebde784efd
AnoMorCH <anomorch@gmail.com>
2024-07-22 17:05:20 +0300

    Init Django project

---
+++ backend/running_text_video_generator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RunningTextVideoGeneratorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'running_text_video_generator'
