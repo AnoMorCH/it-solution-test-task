045ac67f04c5c8eadbce4ea292ea8c2efebd6d57
AnoMorCH <anomorch@gmail.com>
2024-07-24 08:53:52 +0300

    Save sent texts to the database using TextLog model

--- backend/running_text_video_generator/models.py
+++ backend/running_text_video_generator/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class TextLog(models.Model):
+    text = models.TextField()
--- backend/running_text_video_generator/views.py
+++ backend/running_text_video_generator/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.views.generic import View
 from running_text_video_generator.entity.running_line_clip import RunningLineClip
 from backend.settings import MEDIA_PATH
+from .models import TextLog
 from wsgiref.util import FileWrapper
 import os
 import shutil
@@ -25,15 +26,19 @@ class RunningLineClipView(View):
         output = rlc.send_to_client(clip, fmt)
     
         if output[rlc.MSG_KEY] == rlc.SUCCESS_MSG:
-            file_name = f"{rlc.FILE_NAME}.{fmt}"
-            file = FileWrapper(open(f"{MEDIA_PATH}/{file_name}", "rb"))
-            response = HttpResponse(file, content_type=f"video/{fmt}")
-            response["Content-Disposition"] = f"attachment; filename={file_name}"
-            return response
+            TextLog.objects.create(text=text)
+            return self.__prepare_file_response(rlc, fmt)
         else:
             return HttpResponse(json.dumps(output), content_type="application/json", status=400)
 
     def __delete_saved_videos(self) -> None:
         shutil.rmtree(MEDIA_PATH)
         os.makedirs(MEDIA_PATH)
+
+    def __prepare_file_response(self, rlc: RunningLineClip, fmt: str) -> HttpResponse:
+        file_name = f"{rlc.FILE_NAME}.{fmt}"
+        file = FileWrapper(open(f"{MEDIA_PATH}/{file_name}", "rb"))
+        response = HttpResponse(file, content_type=f"video/{fmt}")
+        response["Content-Disposition"] = f"attachment; filename={file_name}"
+        return response
 
