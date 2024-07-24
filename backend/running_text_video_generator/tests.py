641e08f8b61908b605589b88d7357f63c199db70
AnoMorCH <anomorch@gmail.com>
2024-07-24 11:07:59 +0300

    Test RunningLineClipView

--- backend/running_text_video_generator/tests.py
+++ backend/running_text_video_generator/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.http import HttpResponse
 from running_text_video_generator.entity.running_line_clip import RunningLineClip, Message
+from running_text_video_generator.views import RunningLineClipView
 from backend.settings import MEDIA_ROOT
 from moviepy.editor import CompositeVideoClip
 import os
@@ -45,3 +47,42 @@ class RunningLineClipTestCase(TestCase):
     def test_created_video_name_with_diff_fmt(self) -> None:
         self.rlc.send_to_client(self.clip, "webm")
         assert f"{self.rlc.FILE_NAME}.webm" in self.__get_file_names()
+
+
+class RunningLineClipViewTestCase(TestCase):
+    def setUp(self) -> None:
+        self.factory = RequestFactory()
+        self.bad_request = self.factory.get("/running_text_video_generator")
+        self.text_request = self.factory.get("/running_text_video_generator?text=Hello world")
+        self.text_fmt_request = self.factory.get("/running_text_video_generator?text=Hello world&fmt=webm")
+        self.text_wrong_fmt_request = self.factory.get("/running_text_video_generator?text=Hello world&fmt=hahah")
+        self.bad_response = RunningLineClipView.as_view()(self.bad_request)
+        self.text_response = RunningLineClipView.as_view()(self.text_request)
+        self.text_fmt_response = RunningLineClipView.as_view()(self.text_fmt_request)
+        self.text_wrong_fmt_response = RunningLineClipView.as_view()(self.text_wrong_fmt_request)
+
+    def test_type(self) -> None:
+        assert type(self.bad_response) is HttpResponse
+        assert type(self.text_response) is HttpResponse
+        assert type(self.text_fmt_response) is HttpResponse
+        assert type(self.text_wrong_fmt_response) is HttpResponse
+
+    def test_http_code(self) -> None:
+        assert self.bad_response.status_code == 400
+        assert self.text_response.status_code == 200
+        assert self.text_fmt_response.status_code == 200
+        assert self.text_wrong_fmt_response.status_code == 400
+
+    def test_content_type(self) -> None:
+        assert self.bad_response["Content-Type"] == "application/json"
+        assert self.text_response["Content-Type"] == "video/mp4"
+        assert self.text_fmt_response["Content-Type"] == "video/webm"
+        assert self.text_wrong_fmt_response["Content-Type"] == "application/json"
+
+    def test_if_files_are_deleted(self) -> None:
+        files_before_video_generator = [f for f in os.listdir(MEDIA_ROOT)]
+        self.text_fmt_request = self.factory.get("/running_text_video_generator?text=Hello world&fmt=webm")
+        files_after_video_generator = [f for f in os.listdir(MEDIA_ROOT)]
+        assert len(files_before_video_generator) == 0
+        assert len(files_after_video_generator) == 0
+    
